c932b416b45a696169139c4ccf41b0ce6d205058
ednakatani <eduardonakatani87@gmail.com>
2022-04-25 20:57:06 -0300

    estrutura básica

--- monitor.py
+++ monitor.py
@@ -1,1 +1,55 @@
 ## Recebe as informações dos sensores
+
+import socket
+import sys
+import threading
+
+
+# === FUNÇÕES ===
+
+def TrataSensor(conn):
+    while True:
+        data = conn.recv(1000)
+        if not data:
+            break
+        else:
+            print('recebi ', len(data), ' bytes')
+            print(data)
+
+    conn.close()    
+    print('Sensor encerrado: ', conn)
+
+
+# === PROGRAMA PRINCIPAL ===
+
+HOST = ''       # ANY_IP = todos os IPs do HOST
+SENSORES={}     # lista de sensores conectados
+CONSOLE=None    # conexao com o console remoto
+
+PORTA = int(input('Porta do servidor > '))
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((HOST, PORTA))
+except:
+   print('# erro de bind')
+   sys.exit()
+
+s.listen(2)
+
+print('Aguardando conexoes porta: ', PORTA)
+
+
+# === LOOP ===
+
+while True:
+    conn, addr = s.accept()
+    print('Conexão recebida: ',)
+    TrataSensor(conn) 
+    print('Sensor encerrou a conexão')
+    conn.close()
+# === FIM LOOP ===
+
+
+print('o servidor encerrou')
+s.close()
--- sensor.py
+++ sensor.py
@@ -1,1 +1,32 @@
 ## Envia os dados para o monitor
+
+import socket
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+IP = input('IP do servidor > ')
+PORTA = int(input('Porta do servidor > '))
+ID = input('ID do sensor > ')
+
+try:
+    s.connect((IP, PORTA))
+except:
+    print('Erro de conexão')
+
+# Envia o ID automaticamente após a conexão
+s.send(bytes(ID, 'utf-8'))
+
+while True:
+    try:
+        line = input()
+        if not line:
+            print('Linha vazia encerra o programa')
+            break
+    except:
+            print('programa abortado com CTRL+C')
+    data = bytes(line, 'utf-8')
+    tam = s.send(data)
+           
+    print('Enviado:  ',tam, 'bytes')
+    print(data)
